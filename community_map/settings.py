f7a718438f7fb1c2ee8b505ec65b44a14157b841
KriXsh-09 <kkakrot09@gmail.com>
2025-08-29 21:55:18 +0530

    Update settings.py

--- community_map/settings.py
+++ community_map/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-onz(ac)b1bgd64*pn=ttu!yxkily!z@r&hcke_u49)7ld-nq6i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")
 
-ALLOWED_HOSTS = ["community-service-finder.onreader.com","localhost", "127.0.0.1"]
 
 
 # Application definition
